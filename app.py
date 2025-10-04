3bb40a1092afa9beda09a990a1331f4c22ddfef8
Hhoaa <169076770+Hhoaa@users.noreply.github.com>
2025-10-04 21:38:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -681,6 +681,8 @@ def health():
 
 if __name__ == "__main__":
     # set GEMINI_API_KEY=... && set SUPABASE_URL=... && set SUPABASE_ANON_KEY=... && python app_gemini_product_search.py
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
 
 
